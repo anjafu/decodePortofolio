23c0929ff082c325cbeb0e59f3d5517d67414829
anjafu <anjafu@uia.no>
2026-04-06 12:14:03 +0200

    Added mocha chai + gitignore

---
+++ test/brailleTranslator.test.mjs
@@ -0,0 +1,9 @@
+import {expect} from "chai";
+import { it } from "mocha";
+
+describe("'Translate braille binary' function tests: ", () => {
+
+    it("translateBrailleBinary() function should be a function", () => {
+        expect(translateBrailleBinary).to.be.a('function');
+    });
+});
