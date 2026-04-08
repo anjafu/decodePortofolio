aee2b4743f410993869ab1374e221147146e8c04
anjafu <anjafu@uia.no>
2026-04-08 15:39:27 +0200

    Made base64Translator check for corrupted signs

--- src/base64Translator.mjs
+++ src/base64Translator.mjs
@@ -1,6 +1,8 @@
 function decodeBase64(base64String){
     const buffer = Buffer.from(base64String, 'base64');
-    return buffer.toString('utf-8');
+    const rawOutput = buffer.toString('utf-8'); //changed the text to utf-8 BUT doesnt check for corrupt data
+
+    return rawOutput.match(/^[\x20-\x7E\s]*/)[0].trim();//removes corrupted data
 }
 
 export {decodeBase64}
